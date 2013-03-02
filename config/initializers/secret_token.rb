e5494935f7461b7bae3122e994353dc88a599fd4
Kathy Bui <kathybui@alum.mit.edu>
2013-03-02 11:45:01 -0500

    Added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = '6364af7ccf0b1d5f74fa6dfe300b20ac1de99f8688bebda2f365db19db69a68cf9ec21d3e49b29121c3f413eed2a6fbe8d0711c0c6a12f353a809cada04cdbfe'
